3de5684d96341df63bb6c96979d127ad70a08e33
includeovalle <ovalle.carlosr@gmail.com>
2023-05-25 12:56:57 -0600

    remove console logs, add no chache on fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import { Tasks } from './components/Designs/index'
 
 export default async function Home() {
-
     //add ServerApi from .env.local file
-    const res = await fetch(`${process.env.SERVER_API}/tasks`)
+    const res = await fetch(`${process.env.SERVER_API}/tasks`,{ cache: 'no-store' })
     const data = await res.json()
 
     console.log(data)
