c549fddcc769d850fdb3b8ae7f32e8cb8659bcd3
includeovalle <ovalle.carlosr@gmail.com>
2023-05-25 19:31:39 -0600

    update cache handler

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Tasks } from './components/Designs/index'
 
 export default async function Home() {
     //add ServerApi from .env.local file
-    const res = await fetch(`${process.env.SERVER_API}/tasks`,{ cache: 'max-age=0, must-revalidate' })
+    const res = await fetch(`${process.env.SERVER_API}/tasks`,{ cache: 'no-store' })
     const data = await res.json()
 
     return (
