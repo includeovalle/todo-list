a44e694f7727a56b970249db9523ad1a76e6ad53
includeovalle <ovalle.carlosr@gmail.com>
2023-05-30 20:28:03 -0600

    update label interface

--- app/types/label/index.ts
+++ app/types/label/index.ts
@@ -4,4 +4,5 @@ export default interface Props extends childrenInterface, classNamesInterface {
     type: "submit" | "text" | "password" | "email" | "number" | "search" | "checkbox" | "radio" | "file" | "date" | "datetime-local" | "hidden" | "image" | "month" | "range" | "reset" | "tel" | "time" | "url" | "week";
     name: string;
     placeholder?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
