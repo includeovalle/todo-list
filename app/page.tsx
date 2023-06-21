e6acda7299fe137c0e82a1f19559115c7e543c97
Carlos Amaro <71602132+includeovalle@users.noreply.github.com>
2023-06-20 23:16:40 -0600

    Merge pull request #23 from includeovalle/master (#24)
    * update post config
    
    * update next version, add taks route, put together routes get and pot
    
    * update post and get requests

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { Tasks } from './components/Designs/index'
 
 export default async function Home() {
+    const response = await fetch('http://50.116.17.7:3005/tasks', {  cache: 'no-store' })
+    const data = await response.json()
 
-    const res = await fetch("http://localhost:3000/api/task", { cache: 'no-store' })
-    const data = await res.json()
-
+    
     return (
         <>
         <Tasks rows={10} 
